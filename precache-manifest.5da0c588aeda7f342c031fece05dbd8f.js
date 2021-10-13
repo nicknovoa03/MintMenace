c6b8276e2c0a4d2dd5cf45581a5c55da91816ac5
nicknovoa03 <nicknovoa03@gmail.com>
2021-10-12 22:15:52 -0500

    Updates

---
+++ precache-manifest.5da0c588aeda7f342c031fece05dbd8f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794f54913c5f635caefae67737cb10a9",
+    "url": "/MintMenace/index.html"
+  },
+  {
+    "revision": "029011a940cdebaa4fad",
+    "url": "/MintMenace/static/js/2.f956d393.chunk.js"
+  },
+  {
+    "revision": "e038e461caf9b40d27d2",
+    "url": "/MintMenace/static/js/main.acc4f922.chunk.js"
+  },
+  {
+    "revision": "2b0aee6db822a8fb45e5",
+    "url": "/MintMenace/static/js/runtime-main.3a70182f.js"
+  }
+]);
