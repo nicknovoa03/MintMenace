f4927f53ab4073cb8dc902302a06350bc87df98c
andrewnoe <andrewnoe60@gmail.com>
2021-10-12 21:48:51 -0500

    Revert "Updates"
    This reverts commit d0653a5e9594e42213d804d8c8c362c254aa2807.

---
+++ precache-manifest.be430d982d9cb082b6382558590d330e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e85e952d2def5579819f5a70b92dae4",
+    "url": "/mintmenace/index.html"
+  },
+  {
+    "revision": "029011a940cdebaa4fad",
+    "url": "/mintmenace/static/js/2.f956d393.chunk.js"
+  },
+  {
+    "revision": "cd8bda177a2cd2b6900f",
+    "url": "/mintmenace/static/js/main.faae815b.chunk.js"
+  },
+  {
+    "revision": "3dd8f6e98a3e33234655",
+    "url": "/mintmenace/static/js/runtime-main.ca011534.js"
+  }
+]);
